2a496a5a8e25e2a628d89c060198e00f6ffc5d5f
Brandon Morrison <brandon@brandonam.com>
2016-02-03 07:04:32 -0800

    seeding models

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var user = require('../models/users.js');
-
-
+var mongoose = require('mongoose');
+var userModel = require('../models/users');
+var User = mongoose.model('User', userModel);
 
 /* GET users listing. */
 router.route('/')
 
     // get the user with that id (accessed at GET http://localhost:8080/api/users/:user_id)
     .get(function(req, res) {
-      res.send('get send ' + req.params.id);
+      User.find(function(err, users) {
+            if (err)
+                res.send(err);
+            res.json(users);
+        });
     })
 
     // update the user with this id (accessed at PUT http://localhost:8080/api/users/:user_id)
@@ -21,8 +25,13 @@ router.route('/:id')
 
     // get the user with that id (accessed at GET http://localhost:8080/api/users/:user_id)
     .get(function(req, res) {
-      res.send('get send id ' + req.params.id);
-    })
+      User.findById(req.params.id, function(err, users) {
+            if (err)
+                res.send(err);
+            res.json(users);
+            res.end();
+        })
+      })
 
     // update the user with this id (accessed at PUT http://localhost:8080/api/users/:user_id)
     .put(function(req, res) {
