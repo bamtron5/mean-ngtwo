2fbe5af931fdbc40b5cbf6ac895c33835d97816e
Brandon Morrison <brmorrison61@gmail.com>
2016-02-04 05:55:48 -0800

    built service and mapped to observables in app component.  1 cycle complete

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,33 @@
-import {Component} from 'angular2/core';
+import {Component, View, OnInit} from 'angular2/core';
+import {userService} from './service/user.service';
+import {HTTP_PROVIDERS}    from 'angular2/http';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    selector: 'user',
+    template: `
+    	<ul>
+		    <li *ngFor="#user of users">
+		      {{ user.name }}
+		    </li>
+	    </ul>
+    `,
+	providers: [
+	    HTTP_PROVIDERS,
+	    userService,
+	]
 })
-export class AppComponent { }
+
+export class AppComponent implements OnInit{ 
+	constructor (private _userService: userService) {}
+	users: Array<Object>;
+	errorMessage: String;
+
+	ngOnInit() { this.getUsers(); }
+
+	getUsers(){
+		this._userService.getUsers().subscribe(
+			users => this.users = users,
+			error => this.errorMessage = <any>error
+		);
+	}
+};
