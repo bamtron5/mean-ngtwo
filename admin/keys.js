9a4483b7aeaa056bee816557c2f062476b9676f6
brandon@brandonam.com <brandon@brandonam.com>
2016-02-19 21:11:13 -0800

    adding keys chaning mongodb connect

---
+++ admin/keys.js
@@ -0,0 +1,1 @@
+module.exports = { PUBLIC_KEY:'6LdlpxgTAAAAAErP-SbdYA6A7Cc2dfGxWewWm-XX', PRIVATE_KEY:'6LdlpxgTAAAAAErtFPXWFiURdVEhk9pSA4t75VY2'}
--- admin/mongoConnect.js
+++ admin/mongoConnect.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
 
 //env dependent
-module.exports = mongoose.connect('mongodb://heroku_0n5b3krj:56c60a16aa072a665724a28b@ds049744.mongolab.com:49744/heroku_0n5b3krj');
+module.exports = mongoose.connect('mongodb://heroku_0n5b3krj:hv00uqljiljjuqg1jn37hpv8e@ds049744.mongolab.com:49744/heroku_0n5b3krj');
+// mongodb://heroku_0n5b3krj:56c60a16aa072a665724a28b@ds049744.mongolab.com:49744/heroku_0n5b3krj
+// mongodb://heroku_0n5b3krj:hv00uqljiljjuqg1jn37hpv8e@ds049744.mongolab.com:49744/heroku_0n5b3krj
