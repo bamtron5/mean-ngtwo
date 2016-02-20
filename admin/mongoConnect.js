88a1325fa659e3ffcc836c611ed8cca570ba1d5e
brandon@brandonam.com <brandon@brandonam.com>
2016-02-19 20:30:20 -0800

    detch to private branch

--- admin/mongoConnect.js
+++ admin/mongoConnect.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 
 //env dependent
-module.exports = mongoose.connect('mongodb://localhost/claimBook');
+module.exports = mongoose.connect('mongodb://heroku_0n5b3krj:Solutions827!@ds049744.mongolab.com:49744/heroku_0n5b3krj');
